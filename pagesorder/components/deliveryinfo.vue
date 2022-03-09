b594c7a92a75b4380c95914ead19402b2fe03b1d
sin lee <leesin350541732@gmail.com>
2022-03-09 16:08:30 +0800

    Initial commit

---
+++ pagesorder/components/deliveryinfo.vue
@@ -0,0 +1,46 @@
+/**
+ * 订单结算页下配送信息组件 
+ */
+<template>
+  <view class="container">
+      <view class="header">
+        <view v-if="item.canDelivery === 1" class="header-item" :class="{'tabactive':item.seltab===1}" @click="changeTab(1)">配送到家</view>
+        <view class="header-item" :class="{'tabactive':item.seltab===2}" @click="changeTab(2)">到店自取</view>
+      </view>
+      <view class="content">
+        <!-- 骑手配送 -->
+        <view v-if="item.canDelivery === 1 && item.seltab===1" class="delivery">
+          <view class="leftflex border" @click="gotoMyAddress">
+            <view class="common-flex">
+              <view class="common-bigtitle common-bluetitle common-padding ">{{ item.selAddress }}</view>
+            </view>
+            <view>
+              <!-- 注释掉此代码正常 -->
+              <!-- <u-icon name="arrow-right" size="36rpx" color="#757575"></u-icon> -->
+            </view>
+          </view>
+        </view>
+      </view>   
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        default: function () {
+          return {}
+        }
+      },
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pagesorder/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <view>
+    <view>我是父组件</view>
+    <deliveryinfo :item="item"></deliveryinfo>
+  </view>
+</template>
+
+<script>
+  import deliveryinfo from '../components/deliveryinfo.vue'
+  export default {
+    components: {
+      deliveryinfo
+    },
+    data() {
+      return {
+        item: {
+          canDelivery: 1,
+          selAddress: "选择收货地址",
+          selfContactResponse: {
+            contactPerson: "李",
+            contactPhone: "15269241990"
+          },
+          seltab: 1,
+          shopAddress: "青岛市市北区伊春路104、106号"
+        }
+      }
+    },
+    onLoad() {
+    }
+  }
+</script>
+
+<style>
+</style>
